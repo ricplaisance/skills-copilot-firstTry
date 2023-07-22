50b74ab191c9735510b004872a1afbc8f6c99e1d
ricplaisance <140022826+ricplaisance@users.noreply.github.com>
2023-07-22 10:46:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember() {
+    var skills = document.getElementById("skills");
+    var skillsvalue = skills.options[skills.selectedIndex].value;
+    var skillsname = skills.options[skills.selectedIndex].text;
+    var skillslist = document.getElementById("skillslist");
+    var skillslistvalue = skillslist.value;
+    var skillslistname = skillslist.options[skillslist.selectedIndex].text;
+    var skillslisttext = skillslist.options[skillslist.selectedIndex].text;
+    var skillslisttext = skil
